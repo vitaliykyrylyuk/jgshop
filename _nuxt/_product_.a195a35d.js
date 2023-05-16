90ae22d824d333565c368e01fe4fa4e837e5b563
Vitalii_Kyryliuk <Vitalii_Kyryliuk@epam.com>
2023-05-16 19:17:35 +0300

    2473935

---
+++ _nuxt/_product_.a195a35d.js
@@ -0,0 +1,1 @@
+import{a as t,t as a}from"./entry.eb1e90e5.js";const e={};function o(r,s){return a(r.$route.params)}const n=t(e,[["render",o]]);export{n as default};
---
+++ _nuxt/config.4e759069.js
@@ -0,0 +1,1 @@
+import{N as p,l as o,O as i}from"./entry.eb1e90e5.js";const f={},t=p(f);function s(){const n=o();return n._appConfig||(n._appConfig=i(t)),n._appConfig}export{s as u};
