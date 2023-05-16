2cc722ea26ef0935195ce296440c131192546eaf
Vitalii_Kyryliuk <Vitalii_Kyryliuk@epam.com>
2023-05-16 22:33:24 +0300

    1af03f5

---
+++ _nuxt/about.6937f64a.js
@@ -0,0 +1,1 @@
+import{a as t,b as o,t as a,o as c}from"./entry.eb1e90e5.js";const n={};function r(e,s){return c(),o("div",null,a(e.$t("welcome")),1)}const _=t(n,[["render",r]]);export{_ as default};
