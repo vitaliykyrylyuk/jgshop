857b4227bd1c6401c75aa73b30283c0a14d90d71
Vitalii_Kyryliuk <Vitalii_Kyryliuk@epam.com>
2023-05-29 19:50:53 +0300

    Remove lazy behavior

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -8,15 +8,9 @@ const { isMobile } = useDevice()
     <div
       class="flex flex-wrap lg:max-w-screen-lg lg:mx-auto relative z-20 w-full lg:h-20 items-center justify-between bg-white py-3.5 px-5 space-y-2 md:space-y-0"
     >
-      <HeaderMobileMenu v-if="!isMobile" class="md:hidden" />
+      <LazyHeaderMobileMenu v-if="!isMobile" class="md:hidden" />
       <HeaderLogo class="mr-5" />
-      <div class="flex space-x-4 ml-auto sm:order-2 lg:order-3">
-        <HeaderControl icon="AccountIcon" :label="t('header.signIn')" />
-        <HeaderControl icon="CompareIcon" icon-size="24" badge-text="17" class="hidden sm:flex" />
-        <HeaderControl icon="WishlistIcon" badge-text="17" label="17 $" class="hidden sm:flex" />
-        <HeaderControl icon="CartIcon" badge-text="17" label="17 $" />
-      </div>
-      <HeaderCatalog v-if="!isMobile" class="ml-auto hidden lg:flex" />
+      <LazyHeaderCatalog v-if="!isMobile" class="ml-auto hidden lg:flex" />
       <HeaderSearch class="md:flex md:ml-auto lg:ml-0 sm:order-2 md:order-1 lg:order-2" />
       <HeaderCategories class="lg:flex m-auto sm:order-3 lg:order-1" />
     </div>
