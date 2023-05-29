02e49dd7348117e02c58913ef9062e693e8f4c47
Vitalii_Kyryliuk <Vitalii_Kyryliuk@epam.com>
2023-05-29 19:16:51 +0300

    Create config for deploy to GH pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,8 +62,7 @@ export default defineNuxtConfig({
   },
 
   device: {
-    refreshOnResize: true,
-    defaultUserAgent: 'Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.39 Mobile Safari/537.36'
+    refreshOnResize: true
   },
 
   plugins: [
